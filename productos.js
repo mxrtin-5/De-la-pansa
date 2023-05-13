3b401711a3d479a5691d7cfba7d03ebd3bf52c96
mxrtin-5 <martineluney023@gmail.com>
2023-05-13 15:55:10 -0300

    commit

--- productos.js
+++ productos.js
@@ -13,7 +13,7 @@ const hamburguesa2 = new Producto ('2', 'Homenaje big mac','no se', 2640, 1);
 const hamburguesa3 = new Producto ('3', 'De la huerta','no se', 2640, 1);
 const hamburguesa4 = new Producto ('4', 'De la panza','no se', 3100, 1);
 const hamburguesa5 = new Producto ('5', 'Stacker','no se', 2640, 1);
-const hamburguesa6 = new Producto ('6', 'pesto','no se', 2580, 1);
+const hamburguesa6 = new Producto ('6', 'Pesto','no se', 2580, 1);
 const hamburguesa7 = new Producto ('7', 'Tana','no se', 2470, 1);
 const hamburguesa8 = new Producto ('8', 'Smashing onion','no se', 2580, 1);
 const hamburguesa9 = new Producto ('9', 'Simple cheese', 'no se ', 2000, 1);
@@ -23,4 +23,13 @@ const hamburguesa12 = new Producto ('12', 'Doble cheese','no se ', 2360, 1);
 const hamburguesa13 = new Producto ('13', 'Fija','no se ', 2580, 1);
 const hamburguesa14 = new Producto ('14', 'Big classic', 'no se', 580, 1);
 
-const misHamburguesas = [hamburguesa1, hamburguesa2, hamburguesa3, hamburguesa4, hamburguesa5, hamburguesa6,hamburguesa7,hamburguesa8, hamburguesa9, hamburguesa10, hamburguesa11, hamburguesa12, hamburguesa13, hamburguesa14]
+const misHamburguesas = [hamburguesa1, hamburguesa2, hamburguesa3, hamburguesa4, hamburguesa5, hamburguesa6,hamburguesa7,hamburguesa8, hamburguesa9, hamburguesa10, hamburguesa11, hamburguesa12, hamburguesa13, hamburguesa14];
+
+const extra1 = new Producto ('15', 'Dedos de muzzarella', 'no se', 2070, 1);
+const extra2 = new Producto ('16', 'Aros de cebolla', 'no se', 2070, 1);
+const extra3 = new Producto ('17', 'Papas', 'no se', 2000, 1);
+const extra4 = new Producto ('18', 'Papas con chedar', 'no se', 2400, 1);
+const extra5 = new Producto ('19', 'Papas Panza', 'no se', 2180, 1);
+const extra6 = new Producto ('20', 'Panzada', 'no se', 5100, 1);
+
+const extras = [extra1, extra2, extra3, extra4, extra5,extra6];
--- script.js
+++ script.js
@@ -47,9 +47,9 @@ const bebidas = document.getElementById('bebidas');
 const shop = document.querySelector('.shop-content');
 
 function carne() {
+    shop.innerHTML = ''
     misHamburguesas.forEach((producto) => {
         const divProducto = document.createElement('div');
-        divProducto.innerHTML = ''
         divProducto.className = 'shop-content';
         divProducto.innerHTML = `
         <div class="products">
@@ -58,13 +58,33 @@ function carne() {
             <div class="price">
                 <h4>${producto.nombre}</h4>
                 <h5>${producto.descripcion}</h5>
-                <p>${producto.precio}</p>
+                <p>$${producto.precio}</p>
                 <button>Compra</button>
             </div>
         </div>
         `
-
         shop.append(divProducto)
+    })
+}
+
+function misExtras(){
+    shop.innerHTML = ''
+    extras.forEach((product)=>{
+        const divExtras = document.createElement('div');
+        divExtras.className = 'shop-content';
+        divExtras.innerHTML = `
+        <div class="products">
+        <div class="row">
+            <img src="img/${product.id}.jpg" alt="">
+            <div class="price">
+                <h4>${product.nombre}</h4>
+                <h5>${product.descripcion}</h5>
+                <p>$${product.precio}</p>
+                <button>Compra</button>
+            </div>
+        </div>
+        `
+        shop.append(divExtras);
     })
 }
 
@@ -72,23 +92,23 @@ hamburguesas.addEventListener('click', () => {
     carne()
 });
 
-extras.addEventListener('click', () =>{
+extras.addEventListener('click', () => {
+    misExtras()
+});
+
+acompa.addEventListener('click', () => {
+    misExtras()
+});
+
+veggies.addEventListener('click', () => {
     carne()
 });
 
-acompa.addEventListener('click', ()=>{
+cervezas.addEventListener('click', () => {
     carne()
 });
 
-veggies.addEventListener('click', ()=>{
-    carne()
-});
-
-cervezas.addEventListener('click', ()=>{
-    carne()
-});
-
-bebidas.addEventListener('click', ()=>{
+bebidas.addEventListener('click', () => {
     carne()
 });
 
